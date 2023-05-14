c711e2bb9c24402a57043c327aab23d6632b0cd0
homebeach <petri.kotiranta@gmail.com>
2023-05-14 22:03:21 +0300

    More exercises

---
+++ osa2/t2.20/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Filter from './Filter';
+import Countries from './Countries';
+import Country from './Country'; // Import Country component
+import './index.css';
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [note, setNote] = useState("");
+
+  return (
+    <Router>
+      <div>
+        {note}
+        <Filter setCountries={setCountries} setNote={setNote} />
+        <Countries countries={countries} />
+        <Switch>
+          <Route path="/country/:countryName" component={Country} /> {/* Corrected Route path and added the Country component */}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ osa2/t2.20/src/Country.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const Country = () => {
+  const { countryName } = useParams();
+  const flagUrl = `https://www.countryflags.com/wp-content/uploads/${countryName}-flag-png-large.png`;
+
+  const baseUrl = 'https://restcountries.com/v3.1/name/';
+  const fields = 'name,capital,area,languages';
+  const url = `${baseUrl}${countryName}?fields=${fields}`;
+
+
+  const [weatherInfo, setWeatherInfo] = useState({});
+
+  const [country, setCountry] = useState({
+    name: { common: '' },
+    capital: '',
+    area: '',
+    languages: {},
+  });
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCountry(response.data[0]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [url]);
+
+  const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + country.capital + "," + country.name.common + "&units=metric&appid=3ccc2416686687acca73e166671633a3";
+
+  useEffect(() => {
+    axios
+      .get(weatherUrl)
+      .then((response) => {
+        setWeatherInfo(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [weatherUrl]);
+
+  const weatherArray = weatherInfo.weather;
+
+  let icon = "";
+  if (Array.isArray(weatherArray) && weatherArray.length > 0) {
+    icon = weatherArray[0]?.icon ?? "unknown"; // "03d"
+  }
+  
+  let iconUrl = "";
+  if (icon !== "unknown") {
+    iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  }
+  return (
+    <div>
+      {country.name &&
+        <div>
+          <h2>{country.name.common}</h2>
+          <p>Capital: {country.capital}</p>
+          <p>Area: {country.area}</p>
+          <h3>Languages</h3>
+          <ul>
+            {Object.entries(country.languages).map(([code, name]) => (
+              <li key={code}>
+                {name} ({code})
+              </li>
+            ))}
+          </ul>
+          <img src={flagUrl} alt={`Flag of ${country.name.common}`} />
+
+          <h2>Weather in {country.name.common}</h2>
+          <p>temperature: {weatherInfo?.main?.temp} Celsius</p>
+
+          <img src={iconUrl} />
+
+          <p>wind: {weatherInfo?.wind?.speed} m/s</p>
+
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Country;
